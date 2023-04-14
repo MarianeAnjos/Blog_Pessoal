b25e96e0a84dc7e34ea38bb212936195d3fa7357
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-04-13 22:32:18 -0300

    Adição do @Repository

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.repository;
 
+//@Repository
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.blogpessoal.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
