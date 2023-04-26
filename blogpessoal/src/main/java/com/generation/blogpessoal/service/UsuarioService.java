43d50cb1de824d0855cc1e31aeccb1a058e2f550
Mariane Anjos <100323487+MarianeAnjos@users.noreply.github.com>
2023-04-26 16:39:58 -0300

    Adição do pacote de serviço e classe Usuário Service, inicio da criptografia de usuario e senha

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package com.generation.blogpessoal.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.security.JwtService;
+
+
+//Regras de negócio especificas da aplicação, criptografia de senha 
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+        if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+            return Optional.empty();
+
+        usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+        return Optional.of(usuarioRepository.save(usuario));
+
+    }
+
+    private String criptografarSenha(String senha) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder.encode(senha);
+
+    }
+}
